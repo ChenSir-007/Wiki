87182b56a97dce5ffca0798d47f9376fa0be2a30
Jack_Chen <cjl1179822675@163.com>
2022-04-18 22:44:38 +0800

    2-3 Mybatis使用示例：开发/test/list接口，查询test表所有数据

--- src/main/java/com/jackchen/wiki/config/WikiApplication.java
+++ src/main/java/com/jackchen/wiki/config/WikiApplication.java
@@ -1,5 +1,6 @@
 package com.jackchen.wiki.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +10,7 @@ import org.springframework.core.env.Environment;
 
 @ComponentScan("com.jackchen")
 @SpringBootApplication
+@MapperScan("com.jackchen.wiki.mapper")
 public class WikiApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
---
+++ src/main/java/com/jackchen/wiki/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package com.jackchen.wiki.mapper;
+
+import com.jackchen.wiki.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+
+    public List<Test> list();
+}
