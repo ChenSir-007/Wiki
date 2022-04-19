2fb3b5e8ce0daaa6bcecfdff2e66320145baf4b6
Jack_Chen <cjl1179822675@163.com>
2022-04-19 20:25:03 +0800

    2-5 增加demo表列表查询

---
+++ src/main/java/com/jackchen/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.jackchen.wiki.service;
+
+import com.jackchen.wiki.domain.Demo;
+import com.jackchen.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+}
