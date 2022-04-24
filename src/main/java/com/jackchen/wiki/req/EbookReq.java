150aa3c8f98c8578423761de0419f25c246ec893
Jack_Chen <cjl1179822675@163.com>
2022-04-24 16:25:08 +0800

    5-5 封装分页请求参数PageReq和返回参数PageResp

--- src/main/java/com/jackchen/wiki/req/EbookReq.java
+++ src/main/java/com/jackchen/wiki/req/EbookReq.java
@@ -1,6 +1,6 @@
 package com.jackchen.wiki.req;
 
-public class EbookReq {
+public class EbookReq extends PageReq{
     private Long id;
 
     private String name;
--- src/main/java/com/jackchen/wiki/service/EbookService.java
+++ src/main/java/com/jackchen/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.jackchen.wiki.domain.EbookExample;
 import com.jackchen.wiki.mapper.EbookMapper;
 import com.jackchen.wiki.req.EbookReq;
 import com.jackchen.wiki.resp.EbookResp;
+import com.jackchen.wiki.resp.PageResp;
 import com.jackchen.wiki.util.CopyUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,13 +25,13 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<EbookResp> list(EbookReq req) {
+    public PageResp<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if(!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%" + req.getName() + "%");
         }
-        PageHelper.startPage(1,3);
+        PageHelper.startPage(req.getPage(), req.getSize());
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
         PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
@@ -44,6 +45,9 @@ public class EbookService {
 //            respList.add(ebookResp);
 //         }
         List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
-        return list;
+        PageResp<EbookResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
     }
 }
