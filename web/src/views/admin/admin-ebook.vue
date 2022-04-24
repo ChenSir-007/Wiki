f80c72916d800bc838b9d89244c75cec572a3ab3
Jack_Chen <cjl1179822675@163.com>
2022-04-24 21:35:05 +0800

    5-9 增加了雪花算法工具类，完成电子书新增功能

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -3,6 +3,9 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
+      <a-button type="primary" @click="add()" size="large">
+        新增
+      </a-button>
       <a-table
           :columns="columns"
           :row-key="record => record.id"
@@ -168,6 +171,14 @@ export default defineComponent({
       ebook.value = record
     };
 
+    /**
+     * 新增
+     */
+    const add = () => {
+      modalVisible.value = true;
+      ebook.value = {};
+    };
+
     onMounted(() => {
       handleQuery({
         page:1,
@@ -182,6 +193,7 @@ export default defineComponent({
       loading,
       handleTableChange,
       edit,
+      add,
       modalVisible,
       modalLoading,
       handleModalOk,
