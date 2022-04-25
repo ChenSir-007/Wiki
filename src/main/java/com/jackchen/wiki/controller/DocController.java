b6497cfac9cccfe5ad44a05e3e516efba5a3a533
Jack_Chen <cjl1179822675@163.com>
2022-04-25 20:06:00 +0800

    7-2 从分类管理复制出文档管理的前后端代码

---
+++ src/main/java/com/jackchen/wiki/controller/DocController.java
@@ -0,0 +1,51 @@
+package com.jackchen.wiki.controller;
+
+import com.jackchen.wiki.req.DocQueryReq;
+import com.jackchen.wiki.req.DocSaveReq;
+import com.jackchen.wiki.resp.DocQueryResp;
+import com.jackchen.wiki.resp.CommonResp;
+import com.jackchen.wiki.resp.PageResp;
+import com.jackchen.wiki.service.DocService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/doc")
+public class DocController {
+
+    @Resource
+    private DocService docService;
+
+    @GetMapping("/all")
+    public CommonResp all() {
+        CommonResp<List<DocQueryResp>> resp = new CommonResp<>();
+        List<DocQueryResp> list = docService.all();
+        resp.setContent(list);
+        return resp;
+    }
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid DocQueryReq req) {
+        CommonResp<PageResp<DocQueryResp>> resp = new CommonResp<>();
+        PageResp<DocQueryResp> list = docService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody DocSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        docService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        docService.delete(id);
+        return resp;
+    }
+}
---
+++ src/main/java/com/jackchen/wiki/req/DocQueryReq.java
@@ -0,0 +1,8 @@
+package com.jackchen.wiki.req;
+
+public class DocQueryReq extends PageReq {
+    @Override
+    public String toString() {
+        return "DocQueryReq{} " + super.toString();
+    }
+}
