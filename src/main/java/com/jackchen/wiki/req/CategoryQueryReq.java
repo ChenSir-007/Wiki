5ccd5fb9b278dfdb94d4424fb23bac039885c9db
Jack_Chen <cjl1179822675@163.com>
2022-04-25 13:48:59 +0800

    6-2 完成分类管理功能基本的增删改查功能

---
+++ src/main/java/com/jackchen/wiki/req/CategoryQueryReq.java
@@ -0,0 +1,8 @@
+package com.jackchen.wiki.req;
+
+public class CategoryQueryReq extends PageReq {
+    @Override
+    public String toString() {
+        return "CategoryQueryReq{} " + super.toString();
+    }
+}
--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -22,7 +22,7 @@
       <a-table
           :columns="columns"
           :row-key="record => record.id"
-          :data-source="ebooks"
+          :data-source="categorys"
           :pagination="pagination"
           :loading="loading"
           @change="handleTableChange"
@@ -51,26 +51,20 @@
     </a-layout-content>
   </a-layout>
   <a-modal
-      title="电子书表单"
+      title="分类表单"
       v-model:visible="modalVisible"
       :confirm-loading="modalLoading"
       @ok="handleModalOk"
   >
-    <a-form :model="ebook" :label-col="{ span: 6 }" :wrapper-col="{ span:18 }">
-      <a-form-item label="封面">
-        <a-input v-model:value="ebook.cover" />
-      </a-form-item>
+    <a-form :model="category" :label-col="{ span: 6 }" :wrapper-col="{ span:18 }">
       <a-form-item label="名称">
-        <a-input v-model:value="ebook.name" />
+        <a-input v-model:value="category.name" />
       </a-form-item>
-      <a-form-item label="分类一">
-        <a-input v-model:value="ebook.category1Id" />
+      <a-form-item label="父分类">
+        <a-input v-model:value="category.parent" />
       </a-form-item>
-      <a-form-item label="分类二">
-        <a-input v-model:value="ebook.category2Id" />
-      </a-form-item>
-      <a-form-item label="描述">
-        <a-input v-model:value="ebook.description" type="textarea" />
+      <a-form-item label="顺序">
+        <a-input v-model:value="category.sort" />
       </a-form-item>
     </a-form>
   </a-modal>
@@ -83,11 +77,11 @@ import {message} from "ant-design-vue";
 import {Tool} from "../../util/tool";
 
 export default defineComponent({
-  name: 'AdminEbook',
+  name: 'AdminCategory',
   setup() {
     const param = ref();
     param.value = {};
-    const ebooks = ref();
+    const categorys = ref();
     const pagination = ref({
       current: 1,
       pageSize: 10,
@@ -97,35 +91,17 @@ export default defineComponent({
 
     const columns = [
       {
-        title: '封面',
-        dataIndex: 'cover',
-        slots: { customRender: 'cover' }
-      },
-      {
         title: '名称',
         dataIndex: 'name'
       },
       {
-        title: '分类一',
-        key:'category1Id',
-        dataIndex: 'category1Id'
+        title: '父分类',
+        key:'parent',
+        dataIndex: 'parent'
       },
       {
-        title: '分类二',
-        key:'category2Id',
-        dataIndex: 'category2Id'
-      },
-      {
-        title: '文档数',
-        dataIndex: 'docCount'
-      },
-      {
-        title: '阅读数',
-        dataIndex: 'viewCount'
-      },
-      {
-        title: '点赞数',
-        dataIndex: 'voteCount'
+        title: '顺序',
+        dataIndex: 'sort'
       },
       {
         title: 'Action',
@@ -139,7 +115,7 @@ export default defineComponent({
      **/
     const handleQuery = (params: any) => {
       loading.value = true;
-      axios.get("/ebook/list", {
+      axios.get("/category/list", {
         params: {
           page:params.page,
           size:params.size,
@@ -149,7 +125,7 @@ export default defineComponent({
         loading.value = false;
         const data = response.data;
         if (data.success) {
-          ebooks.value = data.content.list;
+          categorys.value = data.content.list;
 
           // 重置分页按钮
           pagination.value.current = params.page;
@@ -172,12 +148,12 @@ export default defineComponent({
     };
 
     // -------- 表单 ---------
-    const ebook = ref({});
+    const category = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
-      axios.post("/ebook/save", ebook.value).then((response) => {
+      axios.post("/category/save", category.value).then((response) => {
         modalLoading.value = false;
         const data = response.data; // data = commonResp
         if (data.success) {
@@ -199,7 +175,7 @@ export default defineComponent({
      */
     const edit = (record:any) => {
       modalVisible.value = true;
-      ebook.value = Tool.copy(record);
+      category.value = Tool.copy(record);
     };
 
     /**
@@ -207,11 +183,11 @@ export default defineComponent({
      */
     const add = () => {
       modalVisible.value = true;
-      ebook.value = {};
+      category.value = {};
     };
 
     const handleDelete = (id: number) => {
-      axios.delete("/ebook/delete/" + id).then((response) => {
+      axios.delete("/category/delete/" + id).then((response) => {
         const data = response.data; // data = commonResp
         if (data.success) {
           // 重新加载列表
@@ -231,7 +207,7 @@ export default defineComponent({
     });
 
     return {
-      ebooks,
+      categorys,
       pagination,
       columns,
       loading,
@@ -243,7 +219,7 @@ export default defineComponent({
       modalLoading,
       handleModalOk,
       handleDelete,
-      ebook,
+      category,
       param
     }
   }
