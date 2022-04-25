1f19fa85885bd80434f99dd99c2157922771e150
Jack_Chen <cjl1179822675@163.com>
2022-04-25 12:43:40 +0800

    6-1 分类表设计和持久层代码生成 基本无限树型表结构设计：id+parent

--- doc/all.sql
+++ doc/all.sql
@@ -1,14 +1,24 @@
 # 电子书表
-drop table if exists `ebook`;
-create table `ebook`(
+# drop table if exists `ebook`;
+# create table `ebook`(
+#     `id` bigint not null comment 'id',
+#     `name` varchar(50) comment '名称',
+#     `category1_id` bigint comment '分类1',
+#     `category2_id` bigint comment '分类2',
+#     `description` varchar(200) comment '描述',
+#     `cover` varchar(200) comment '封面',
+#     `doc_count` int comment '文档数',
+#     `view_count` int comment '阅读数',
+#     `vote_count` int comment '点赞数',
+#     primary key (`id`)
+# ) engine=innodb default charset=utf8mb4 comment='电子书';
+
+#分类
+drop table if exists `category`;
+create table `category`(
     `id` bigint not null comment 'id',
-    `name` varchar(50) comment '名称',
-    `category1_id` bigint comment '分类1',
-    `category2_id` bigint comment '分类2',
-    `description` varchar(200) comment '描述',
-    `cover` varchar(200) comment '封面',
-    `doc_count` int comment '文档数',
-    `view_count` int comment '阅读数',
-    `vote_count` int comment '点赞数',
+    `parent` bigint not null default 0 comment '父id',
+    `name` varchar(50) not null comment '名称',
+    `sort` int comment '顺序',
     primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='电子书';
+)engine=innodb default charset=utf8mb4 comment='分类';
