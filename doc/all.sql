f152287018004c09466f844a43df0e69c3c56dd9
Jack_Chen <cjl1179822675@163.com>
2022-04-25 18:01:52 +0800

    7-1 增加文档表SQL，并生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -14,11 +14,24 @@
 # ) engine=innodb default charset=utf8mb4 comment='电子书';
 
 #分类
-drop table if exists `category`;
-create table `category`(
+# drop table if exists `category`;
+# create table `category`(
+#     `id` bigint not null comment 'id',
+#     `parent` bigint not null default 0 comment '父id',
+#     `name` varchar(50) not null comment '名称',
+#     `sort` int comment '顺序',
+#     primary key (`id`)
+# )engine=innodb default charset=utf8mb4 comment='分类';
+
+#文档表
+drop table if exists `doc`;
+create table `doc` (
     `id` bigint not null comment 'id',
+    `ebook-id` bigint not null default 0 comment '电子书id',
     `parent` bigint not null default 0 comment '父id',
     `name` varchar(50) not null comment '名称',
     `sort` int comment '顺序',
+    `view_count` int default 0 comment '阅读量',
+    `vote_count` int default 0 comment '点赞数',
     primary key (`id`)
-)engine=innodb default charset=utf8mb4 comment='分类';
+)engine=innodb default charset=utf8mb4 comment='文档';
