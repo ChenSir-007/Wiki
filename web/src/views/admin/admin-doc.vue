72a4e5652e3ee0bfc8173936d14067425c7e448e
Jack_Chen <cjl1179822675@163.com>
2022-04-26 18:09:07 +0800

    7-10 增加保存文档成功提示，编辑和新增时，先清空富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -199,6 +199,7 @@ export default defineComponent({
         const data = response.data; // data = commonResp
         if (data.success) {
           modalVisible.value = false;
+          message.success('保存成功！')
 
           // 重新加载列表
           handleQuery();
@@ -278,6 +279,8 @@ export default defineComponent({
      * 编辑
      */
     const edit = (record:any) => {
+      //编辑时先清空富文本框
+      editor.txt.html("");
       modalVisible.value = true;
       doc.value = Tool.copy(record);
       handleQueryContent();
@@ -294,6 +297,8 @@ export default defineComponent({
      * 新增
      */
     const add = () => {
+      //编辑时先清空富文本框
+      editor.txt.html("");
       modalVisible.value = true;
       doc.value = {
         ebookId:route.query.ebookId
